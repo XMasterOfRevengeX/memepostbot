1da0a071116b457ebd1d96bd0102de1b7746a0ae
alphuite <contact@alphuite.com>
2016-12-27 21:16:17 +0000

    added basic code

---
+++ bot.js
@@ -0,0 +1,55 @@
+// memebot
+var twitter = require('twitter');
+var keys = require('./configs/keys.js');
+var discord = require('discord.io');
+
+var twitterBot = new twitter({
+    consumer_key: keys.twitterKey,
+    consumer_secret: keys.twitterSecret,
+    access_token_key: keys.twitterAccessToken,
+    access_token_secret: keys.twitterAccessSecret
+});
+
+var twitterStream = twitterBot.stream('statuses/filter', {follow: 3945972795});
+twitterStream.on('data', function(data){
+    //console.log(data);
+    if (data.user.id == 3945972795){
+        if (data.entities !== undefined && data.entities.media !== undefined){
+            console.log(`@${data.user.screen_name} wrote: "${data.entities.media[0].media_url}"`);
+            discordBot.sendMessage({
+                to: '160863919492169729',
+                message: `@${data.user.screen_name} ${data.entities.media[0].media_url}`
+            });
+        }else{
+            console.log(`@${data.user.screen_name} wrote: "${data.text}"`);
+            discordBot.sendMessage({
+                to: '160863919492169729',
+                message: `@${data.user.screen_name} "${data.text}"`
+            });
+        }
+    } else {
+        //console.log("retweet");
+    }
+});
+
+twitterStream.on('error', function(err){
+    throw err;
+});
+
+var discordBot = new discord.Client({
+    token: keys.discordKey,
+    autorun: true
+});
+
+discordBot.on('ready', function(){
+    console.log(`${discordBot.username} with id ${discordBot.id}`);
+});
+
+discordBot.on('message', function(user, userID, channelID, message, event) {
+    if (message === "ping") {
+        discordBot.sendMessage({
+        to: channelID,
+        message: "pong"
+    });
+                        }
+}); 
